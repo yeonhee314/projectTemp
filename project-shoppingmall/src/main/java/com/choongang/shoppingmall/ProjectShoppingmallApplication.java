24604565671f78e37f3e9e7adb29fcba475365be
kimyeonhee <mouse97_@naver.com>
2024-10-04 15:34:07 +0900

    개발환경 구축, 메타데이터 올라가지 않도록 .gitIgnore 추가

---
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/ProjectShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.choongang.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShoppingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShoppingmallApplication.class, args);
+	}
+
+}
