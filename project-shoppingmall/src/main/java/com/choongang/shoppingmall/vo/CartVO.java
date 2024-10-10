e160da349bcd565363945cb4ee84a4a41c06aed7
DESKTOP-M5CUFVP\admin <lnam061@gmail.com>
2024-10-10 11:50:17 +0900

    카트,위시 vo 추가

---
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/vo/CartVO.java
@@ -0,0 +1,15 @@
+package com.choongang.shoppingmall.vo;
+
+import lombok.Data;
+
+@Data
+public class CartVO {
+	private int cart_id; // 장바구니 고유번호
+	private String user_id; // 회원 고유번호
+	private int product_id; // 상품 고유번호
+	private int cart_count; // 수량
+	private int cart_price; // 장바구니 총금액
+	private String product_name; // 상품 이름
+	private int product_price; // 상품 가격
+
+}
---
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/vo/WishVO.java
@@ -0,0 +1,13 @@
+package com.choongang.shoppingmall.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class WishVO {
+	private int wish_id; // 찜 고유번호
+	private String user_id; // 회원 고유번호
+	private int product_id; // 상품 고유번호
+	private Date wish_date;	// 찜한 날짜
+}
