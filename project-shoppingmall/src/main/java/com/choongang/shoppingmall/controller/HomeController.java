b3aa17415acd5438dae25272b4c7fae12831508d
just_tj <zrzr1993@naver.com>
2024-10-10 10:35:21 +0900

    vo커밋(배송지, 문의글, 문의답글)

--- project-shoppingmall/src/main/java/com/choongang/shoppingmall/controller/HomeController.java
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Configuration
 public class HomeController {
 	
-	@GetMapping("/index")
+	@GetMapping("/index.html")
 	public String index() {
 		return "index";
 	}
---
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/vo/Question_commentVO.java
@@ -0,0 +1,14 @@
+package com.choongang.shoppingmall.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Question_commentVO {
+	
+	private int question_cmt_id;	// 문의댓글 번호
+	private int question_id;	// 문의글 번호
+	private String question_cmt; // 문의댓글
+	private Date cmt_regdate;	// 댓글 작성일
+}
