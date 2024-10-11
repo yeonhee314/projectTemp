d8f8b06a571518dd2d034dac9440728439188363
just_tj <zrzr1993@naver.com>
2024-10-11 17:50:57 +0900

    관리자-유저상세조회 폼

--- project-shoppingmall/src/main/java/com/choongang/shoppingmall/controller/AdminController.java
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/controller/AdminController.java
@@ -17,12 +17,12 @@ public class AdminController {
 	@Autowired
 	private UsersBoardService usersBoardService;
 
-	
+	// 관리자 페이지 접근
 	@GetMapping("/admin")
 	public String admin() {
 		return "admin";
 	}
-
+	// 전체 회원 목록
 	@GetMapping(value =  "/admin/users")
 	public String users(Model model) {
 		model.addAttribute("list",usersBoardService.selectAll());
@@ -31,16 +31,18 @@ public class AdminController {
 	}
 	
 	// 회원 상세 조회
-	@GetMapping("/admin/user/details")
-	public String adminUserDetails(int user_id, Model model) {
-		
+	@GetMapping(value = "/admin/user/details")
+	public String adminUserDetail(Integer user_id, Model model) {
+		model.addAttribute("user_id",user_id);
+		model.addAttribute("details", usersBoardService.selectByID(user_id));
 		return "admin-userdetails";
 	}
-	
+	// 상품 관리
 	@GetMapping("/admin/products")
 	public String adminProducts() {
 		return "admin-products";
 	}
+	// 문의 관리
 	@GetMapping("/admin/qna")
 	public String adminQna() {
 		return "admin-qna";
