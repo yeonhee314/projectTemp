30f432d7ec9359029726c4321b9cbaa0cf7ca829
kjh8242 <kjh82428@naver.com>
2024-10-11 12:02:28 +0900

    Order_DetailVO 수정후 커밋

--- project-shoppingmall/src/main/java/com/choongang/shoppingmall/vo/Order_DetailVO.java
+++ project-shoppingmall/src/main/java/com/choongang/shoppingmall/vo/Order_DetailVO.java
@@ -10,12 +10,18 @@ public class Order_DetailVO {
 	private int order_detail_id;	// 주문상세 고유번호
 	private Date paid_date;			// 결제날짜
 	private int total_price;		// 총 결제 가격
-	private int quantity;			// 주문 수량
+	private int quantity;			// 상품 수량
 	private String order_status;	// 주문 상태 
 	private String paid_option;		// 주문 옵션
 	private String paid_name;		// 구매자 이름
 	private String paid_addr;		// 구매자 주소
+	private String product_name; 	// 상품 이름
+	private String thumb;			// 상품 썸네일
+	private String product_status;	// 상품 옵션
+	private int product_price; 		// 상품 가격
+	private int discount;			// 할인율
 	private String pay_method;		// 결제 수단
+	
 	// 외래키 
 	private int order_id; 			// 주문 번호
 	private int user_id;			// 회원 고유번호
