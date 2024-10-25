96bbf416e5c0a8f68a2145003fb2b8fc7e3b47fc
kimyeonhee <mouse97_@naver.com>
2024-10-25 10:16:56 +0900

    위시 디테일 마무리

--- project-shoppingmall/src/main/resources/static/js/slick-custom.js
+++ project-shoppingmall/src/main/resources/static/js/slick-custom.js
@@ -160,11 +160,39 @@
                 dotsClass:'slick3-dots',
                 customPaging: function(slick, index) {
                     var portrait = $(slick.$slides[index]).data('thumb');
+				console.log('portrait :' + portrait);
+				console.log('slick.$slides[index]).data("thumb") :' + $(slick.$slides[index]).data('thumb'));
                     return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
                 },  
             });
         });
-            
+		/*==================================================================
+	    [ Slick4 ]*/
+	$('.wrap-slick4').each(function() {
+		$(this).find('.slick3').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			fade: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 6000,
+
+			arrows: true,
+			appendArrows: $(this).find('.wrap-slick3-arrows'),
+			prevArrow: '<button class="arrow-slick3 prev-slick3"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+			nextArrow: '<button class="arrow-slick3 next-slick3"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+
+			dots: true,
+			appendDots: $(this).find('.wrap-slick3-dots'),
+			dotsClass: 'slick3-dots',
+			customPaging: function(slick, index) {
+				var portrait = $(slick.$slides[index]).data('thumb');
+				console.log('portrait :' + portrait);
+				console.log('slick.$slides[index]).data("thumb") :' + $(slick.$slides[index]).data('thumb'));
+				return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
+			},
+		});
+	});    
                 
 
 })(jQuery);
